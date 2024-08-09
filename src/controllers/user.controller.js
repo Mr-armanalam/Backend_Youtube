f8736d0820c13adcad6d7d4a6073386739af7e94
Mr-armanalam <armanalam78578@gmail.com>
2024-08-09 08:30:20 +0530

    update route and current password complete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -226,8 +226,69 @@ const refreshAccessToken = asyncHandler(async (req, res) =>{
   throw new ApiError(401, error?.message || "invalid refreshtoken")
  }
 
+})
 
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Invalid old password");
+  }
+
+  user.password = newPassword;
+  await user.save({validateBeforeSave:false});
+
+
+  return res.status(200)
+  .json(new ApiResponse(200, {}, "Password changed successfully "))
+
+
+});
+
+
+const getCurrentUser = asyncHandler(async(req, res) => {
+  return res.status(200)
+  .json(200, req.user, "current user fetched successfully")
 
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+
+const updateAccountDetails = asyncHandler(async(req, res) =>{
+  const { fullname, email } = req.body;
+
+  if (!fullname || !email) {
+    throw new ApiError(400, "fullname and email are required");
+  }
+
+  const user = User.findByIdAndUpdate(req.user?._id,
+    {
+      $set: {
+        fullname,
+        email: email,
+      }
+
+    },{new:true}).select("-password")
+
+    return res
+    .status(200)
+    .json(200, user, "User account details updated successfully")
+    .catch((error) => {
+       throw new ApiError(500, error?.message || "Something went wrong")
+     });
+  
+
+})
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+};
